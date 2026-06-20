b846a55212b1ebaf2fd4c50a07b81cd7544a4637
comit <alireza2040afshari@gmail.com>
2026-06-20 20:08:33 +0330

    Start

---
+++ test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+#x = np.random.normal(3 , 0.1 ,100)
+#y = np.random.normal(5 , 0.1 ,100)
+
+
+#plt.scatter(x, y)
+#plt.axis([0, 10, 0, 12])
+#plt.show()
+
+
+#a = np.random.randint(10 , 50 ,(17,12))
+#a = a.ndim
+#print(a)
+#a = np.arange(1, 13)
+#a = a.reshape((3, 4))
+#print(a)
+#first_array = np.random.randint(1, 100 , (3,2))
+#second_array = np.random.randint(1, 7 , (3,2))
+#print(first_array , "*\n" , second_array)
+#third_array = np.concatenate([first_array,second_array] , axis= 1)
+#print(third_array)
